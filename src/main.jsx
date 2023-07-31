a018dc95a85cec971c0253f86394a7bf0cc0b447
Raul Lendines <raullendines@gmail.com>
2023-07-31 19:20:58 +0200

    before navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './css/index.css'
 import './css/TicTacToe.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )
