f4e280556855ed65e48e6e9d0e4bcfc0b8362474
Raul Lendines <raullendines@gmail.com>
2023-08-12 00:05:48 +0200

    Update WinnerModal.jsx

--- src/components/TicTacToe/WinnerModal.jsx
+++ src/components/TicTacToe/WinnerModal.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { Square } from "./Square.jsx"
+import { Square } from "./Square"
 
 export function WinnerModal ({winner, resetGame}){
     if (winner === null) return null
